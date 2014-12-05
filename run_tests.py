69070cba8a5f9135d178b5bdf682e947e8af8d10
Aaron Gunderson <aaron@agundy.com>
2014-12-04 21:38:58 -0500

    Broke code into functions and made importable

--- run_tests.py
+++ run_tests.py
@@ -6,12 +6,13 @@
 
 import os
 import signal
-import subprocess
+from subprocess import check_call, Popen, check_output
 import sys
 import time
 import pip
 import urllib2
 import tarfile
+
 
 DIR_TEST_ENV = "test/env"
 DIR_JENKINS_ENV = 'jenkins-env'
@@ -19,52 +20,88 @@ VERSION_VIRTUALENV = '1.9.1'
 
 os.environ["JENKINS_HOME"] = "jenkins-master"
 
-print "Checking Patches"
-subprocess.check_call('./test/check_patches.sh')
 
-if os.path.exists("./" + DIR_JENKINS_ENV) and os.environ.get('CI') == None:
-    print "Jenkins environment already exists!"
-    while True:
-        user_input = raw_input("Would you like to recreate it? ")
-        if user_input[0].lower() == "y":
-            print "Running setup:"
-            subprocess.call(["./setup.sh", DIR_JENKINS_ENV])
-            break
-        elif user_input[0].lower() == "n":
-            break
+def python(*args):
+    pypath = [os.path.join(DIR_TEST_ENV, 'bin', 'python')]
+    check_call(pypath + list(args))
+
+
+def pip(*args):
+    pippath = [os.path.join(DIR_TEST_ENV, 'bin', 'pip')]
+    check_call(pippath + list(args))
+
+
+def download(url, destination):
+    response = urllib2.urlopen(url)
+    with open(tar_filename, "wb") as code:
+        code.write(response.read())
+
+
+def kill_jenkins():
+    '''
+     Kill all Jenkins processes
+    '''
+    print "Killing Jenkins"
+    pids = check_output(['lsof', '-i:8080', '-t'])
+    pids = pids.strip().split('\n')
+
+    for pid in pids:
+        os.kill(int(pid), signal.SIGKILL)
+
+
+def run_tests():
+    '''
+    Main Function that handles all the tests.
+    '''
+    try:
+        print "Checking Patches"
+        check_call('./test/check_patches.sh')
+
+        if os.path.exists("./" + DIR_JENKINS_ENV) and os.environ.get('CI') == None:
+            print "Jenkins environment already exists!"
+            while True:
+                user_input = raw_input("Would you like to recreate it? ")
+                if user_input[0].lower() == "y":
+                    print "Running setup:"
+                    check_call(["./setup.sh", DIR_JENKINS_ENV])
+                    break
+                elif user_input[0].lower() == "n":
+                    break
+                else:
+                    print "Please answer yes or no."
         else:
-            print "Please answer yes or no."
-else:
-    print "Running setup"
-    subprocess.call(["./setup.sh", DIR_JENKINS_ENV])
+            print "Running setup"
+            check_call(["./setup.sh", DIR_JENKINS_ENV])
 
-print "Starting Jenkins"
-subprocess.Popen(['./start.py', '>', 'jenkins.out'])
+        print "Starting Jenkins"
+        Popen(['./start.py', '>', 'jenkins.out'])
 
-time.sleep(60)
+        time.sleep(60)
 
-if os.path.exists(DIR_TEST_ENV):
-    print "Using virtual environment in ", DIR_TEST_ENV
-else:
-    tar_filename = "virtualenv-"+ VERSION_VIRTUALENV +".tar.gz"
-    print "Creating a virtual environment (version ", VERSION_VIRTUALENV, ") in ", DIR_TEST_ENV
-    f = urllib2.urlopen("https://pypi.python.org/packages/source/v/virtualenv/virtualenv-" + VERSION_VIRTUALENV + ".tar.gz") 
-    with open(tar_filename,"wb") as code:
-        code.write(f.read())
-    tar = tarfile.open(name=tar_filename)
-    tar.extractall(path='.')
-    subprocess.call(["virtualenv-" + VERSION_VIRTUALENV + "/virtualenv.py" ,DIR_TEST_ENV])
+        if os.path.exists(DIR_TEST_ENV):
+            print "Using virtual environment in ", DIR_TEST_ENV
+        else:
+            tar_filename = "virtualenv-" + VERSION_VIRTUALENV + ".tar.gz"
+            print "Creating a virtual environment (version %s) in %s" %  (VERSION_VIRTUALENV, DIR_TEST_ENV)
+            url = "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-" + \
+                VERSION_VIRTUALENV + ".tar.gz"
+            destination = download(url, tar_filename)
 
-virtualenv_path = DIR_TEST_ENV + "/bin/"
+            tar = tarfile.open(name=tar_filename)
+            tar.extractall(path='.')
+            check_call(
+                ["virtualenv-" + VERSION_VIRTUALENV + "/virtualenv.py", DIR_TEST_ENV])
 
-subprocess.call([virtualenv_path+'pip','install','selenium'])
+        pip('install', 'selenium')
 
-subprocess.call([virtualenv_path+'python','test/configuration/save_config.py'])
+        python('test/configuration/save_config.py')
 
+    except:
+        print "Could not activate virtual environment"
+        print "Exiting"
+        sys.exit(1)
 
-print "Killing Jenkins"
-pids = subprocess.check_output(['lsof','-i:8080','-t'])
-pids = pids.strip().split('\n')
+    kill_jenkins()
 
-for pid in pids:
-    os.kill(int(pid), signal.SIGKILL)
+if __name__ == '__main__':
+    run_tests()
